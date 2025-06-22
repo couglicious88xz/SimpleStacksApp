5b9c8aca047ff26f2af7c3c8a42ee563bfc157d3
Scott Lardizabal-Rosen <scott.lardizabal.rosen@gmail.com>
2025-06-21 20:59:06 -0700

    Updated layout with additional views and styling

--- SimpleStacksApp/ContentView.swift
+++ SimpleStacksApp/ContentView.swift
@@ -10,10 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // large globe purple image
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.purple)
+            Text("Hello world!")
+            // large red text on yellow background
+            Text("It's summer time in Los Angeles")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .padding(5)
+                .background(Color.yellow)
+                .cornerRadius(10)
+        HStack {
+            // small, orange dog image on a black background
+                Image(systemName: "dog")
+                .imageScale(.small)
+                .padding(2)
+                .background(Color.black)
+                .foregroundStyle(.orange)
+            }
+            
         }
         .padding()
     }
